03df57f09a38259d53197c22227e275c8a1e7639
Chris Dilger <c.dilger1@gmail.com>
2017-09-04 12:32:20 +1000

    Setup of project

--- SQLFitness/Program.cs
+++ SQLFitness/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using MySql.Data.MySqlClient;
 
 namespace SQLFitness
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //connect to a mysqldb and execute an example query
+
+            
+
+            Console.ReadLine();
         }
     }
 }
