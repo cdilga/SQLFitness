f2a2d3645791b9435ccdc8cd71acfc64c1ea6711
Chris Dilger <c.dilger1@gmail.com>
2017-09-12 15:28:17 +1000

    Update

--- SQLFitness/Program.cs
+++ SQLFitness/Program.cs
@@ -13,28 +13,7 @@ namespace SQLFitness
 
             var connStr = "server=localhost;user=root;password=example;database=world;port=3306;sslmode=none";
             var conn = new MySqlConnection(connStr);
-            try
-            {
-                Console.WriteLine("Connecting");
-                conn.Open();
-                var sql = "SELECT * FROM country";
-                var cmd = new MySqlCommand(sql, conn);
-                var reader = cmd.ExecuteReader();
-                var columns = new List<string>();
-                for (var i = 0; i < reader.FieldCount; i++) {
-                    columns.Add(reader.GetName(i));
-                    Console.WriteLine(reader.GetName(i));
-                }
-                //var testProjection = new Projection(reader);
-
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
-
-            conn.Close();
-            Console.WriteLine("Done.");
+            
             Console.ReadLine();
 
         }
--- SQLFitness/Selection.cs
+++ SQLFitness/Selection.cs
@@ -4,9 +4,12 @@ using System.Text;
 
 namespace SQLFitness
 {
-    public class Selection
+    public class Selection: Projection
     {
         private enum Operator { equal, notEqual, greaterThan, lessThan, greaterThanEqual, lessThanEqual }
+        private string _operator { get; set; }
+        private string _condition { get; set; }
+        private string _column { get; set; }
 
         public Selection ()
         {
@@ -14,7 +17,40 @@ namespace SQLFitness
             //Has a condition (might be a numerical value or something) - sourced from the database
             //Has a value (i.e. attribute name)
         }
-
         
+        public Mutate()
+        {
+            public void Mutate(MySqlConnection conn)
+            {
+                //Has a value for select
+                //Get a database table - pull all possible values in (columns)
+                //Initialise the projection to a random one of these 
+
+                try
+                {
+                    Console.WriteLine("Connecting");
+                    conn.Open();
+                    var sql = "SELECT * FROM country";
+                    var cmd = new MySqlCommand(sql, conn);
+                    var reader = cmd.ExecuteReader();
+
+                    //Get a random column name
+                    var random = new Random();
+                    _column = reader.GetName(random.Next(0, reader.FieldCount));
+
+                    //var testProjection = new Projection(reader);
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+
+                conn.Close();
+                Console.WriteLine("Done.");
+
+            }
+
+        }
     }
 }
