de42ad5b57bca71bb689a076f053df4bd18d6862
cdilga <c.dilger1@gmail.com>
2018-12-17 17:32:24 -0800

    Add StanfordCoreNLP for test

--- src/main/java/jar/App.java
+++ src/main/java/jar/App.java
@@ -1,13 +1,37 @@
 package jar;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import edu.stanford.nlp.coref.data.CorefChain;
+import edu.stanford.nlp.pipeline.*;
+import java.util.*;
+import edu.stanford.nlp.ling.*;
+import edu.stanford.nlp.ie.util.*;
+import edu.stanford.nlp.pipeline.*;
+import edu.stanford.nlp.semgraph.*;
+import edu.stanford.nlp.trees.*;
+import java.util.*;
+
+
+
+public class App {
+
+    public static void main(String[] args) {
+
+        // creates a StanfordCoreNLP object, with POS tagging, lemmatization, NER, parsing, and coreference resolution
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize, ssplit, pos, lemma");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+        // read some text in the text variable
+        String text = "John saved the cat today";
+
+        // create an empty Annotation just with the given text
+        Annotation document = new Annotation(text);
+
+        // run all Annotators on this text
+        List<CoreMap> sentences = document.get(SentencesAnnotation.class);
+        
+        pipeline.annotate(document);
+        System.out.println(document.sentences().get(0).posTags());
     }
+
 }
