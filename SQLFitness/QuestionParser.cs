daac93e68ff60928d1faae4896704789872f6877
Chris Dilger <c.dilger1@gmail.com>
2019-07-18 20:56:20 +1000

    CoreNLP extracts and caches questions

--- SQLFitness/QuestionParser.cs
+++ SQLFitness/QuestionParser.cs
@@ -1,19 +1,11 @@
-﻿using edu.stanford.nlp.ling;
+using edu.stanford.nlp.ling;
 using edu.stanford.nlp.pipeline;
-using edu.stanford.nlp.trees;
 using edu.stanford.nlp.util;
-using java.net;
-using java.sql;
 using java.util;
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using static edu.stanford.nlp.ling.CoreAnnotations;
 
 namespace SQLFitness
@@ -41,14 +33,15 @@ namespace SQLFitness
             // creates a StanfordCoreNLP object, with POS tagging, lemmatization, NER, parsing, and coreference resolution
             Properties props = new Properties();
             props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse");
+            props.setProperty("ner.useSUTime", "0");
             var curDir = Environment.CurrentDirectory;
-            var jarRoot = @"..\..\..\stanford-corenlp-full-2016-10-31";
+            var jarRoot = @"..\..\..\stanford-corenlp-full-2018-10-05\models";
             Directory.SetCurrentDirectory(jarRoot);
             StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
             Directory.SetCurrentDirectory(curDir);
 
             // read some text in the text variable
-            String text = question;
+            string text = question;
 
             // create an empty Annotation just with the given text
             Annotation document = new Annotation(text);
@@ -57,22 +50,24 @@ namespace SQLFitness
             pipeline.annotate(document);
 
             // these are all the sentences in this document
-            // a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
             var sentences = document.get(sentencesAnnotationClass) as AbstractList;
 
             var parse = new List<string>();
 
-            foreach (CoreMap sentence in sentences) {
+            foreach (CoreMap sentence in sentences)
+            {
                 // traversing the words in the current sentence
-                // a CoreLabel is a CoreMap with additional token-specific methods
-                foreach (CoreLabel token in sentence.get(tokensAnnotationClass) as AbstractList) {
+                // a CoreLabel has additional token-specific methods
+                foreach (CoreLabel token in sentence.get(tokensAnnotationClass) as AbstractList)
+                {
                     // this is the text of the token
-                    String word = token.get(textAnnotationClass) as String;
-                    // this is the POS tag of the token
-                    String pos = token.get(partOfSpeechAnnotationClass) as String;
+                    string word = token.get(textAnnotationClass) as string;
+                    //this is the POS tag of the token
+                    string pos = token.get(partOfSpeechAnnotationClass) as string;
                     //extract the nouns
 
-                    if (pos == "NN" || pos == "NNP" || pos == "NNS" || pos == "NNPS") {
+                    if (pos == "NN" || pos == "NNP" || pos == "NNS" || pos == "NNPS")
+                    {
                         parse.Add(word);
                     }
                 }
